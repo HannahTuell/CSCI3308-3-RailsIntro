c22a3450fc671777ec127bdef8956a2a20247d3e
Hannah Tuell <hannah.tuell@colorado.edu>
2013-10-13 19:32:09 -0700

    Finished Assignment - Remembers Settings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,16 +7,39 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # define the @movies to be displayed as either all, or sorted when :order symbol is passed
-    @movies = Movie.find(:all, :order => params[:sort])
-
+    if(params[:sort] && session[:sort] && params[:ratings] && session[:ratings] == nil)
+      @movies = Movie.all
+    end
+    # initialize an instance variable 'redirect' as false (or zero)
+    @redirect = 0  
     # defines an enumerable collection of all possible values of a movie rating
     @all_ratings = Movie.all_ratings
-    unless params[:ratings].nil?
+
+    # 
+    if(params[:sort] == 'title')
+      session[:sort] = params[:sort]
+      @movies = Movie.find(:all, :order =>params[:sort])
+    elsif(params[:sort] == 'release_date')
+      session[:sort] = params[:sort]
+      @movies = Movie.find(:all, :order =>params[:sort])
+    elsif(session.has_key?(:sort) )
+      params[:sort] = session[:sort]
+      @redirect = 1
+    end
+
+    if(params[:ratings] != nil)
+      session[:ratings] = params[:ratings]
       @checked_ratings = params[:ratings].keys
       @movies = Movie.find(:all, :order => params[:sort]).select { |m| @checked_ratings.include?(m.rating) }
-    end  
-
+    elsif(session.has_key?(:ratings) )
+      params[:ratings] = session[:ratings]
+      @redirect =1
+    end
+    
+    if(@redirect ==1)
+      flash.keep
+      redirect_to movies_path(:sort=>params[:sort], :ratings =>params[:ratings] )
+    end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   @all_ratings = ['G', 'PG', 'PG-13', 'R']
   
- # method on a movie that allows us to interact with the movie's ratings
+ # method on a movie that allows us to interact with the movie's possible ratings
   def self.all_ratings
     @all_ratings
   end
