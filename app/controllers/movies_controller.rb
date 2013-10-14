c22f2ef72df9ff6d8c0bc6cab8f107dbb80b5d18
Hannah Tuell <hannah.tuell@colorado.edu>
2013-10-13 19:39:09 -0700

    getting rid of the weird tilde files, maybe...

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,35 +7,37 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if(params[:sort] && session[:sort] && params[:ratings] && session[:ratings] == nil)
-      @movies = Movie.all
-    end
     # initialize an instance variable 'redirect' as false (or zero)
     @redirect = 0  
-    # defines an enumerable collection of all possible values of a movie rating
+    # defines an enumerable collection of all possible values of a movie rating - used for checkboxes
     @all_ratings = Movie.all_ratings
 
-    # 
+    # if the request is to sort by title, then store that in the session and perform the sort
     if(params[:sort] == 'title')
       session[:sort] = params[:sort]
       @movies = Movie.find(:all, :order =>params[:sort])
+    # if the request is to sort by release date, then store that in the session and perform the sort
     elsif(params[:sort] == 'release_date')
       session[:sort] = params[:sort]
       @movies = Movie.find(:all, :order =>params[:sort])
+    # but if there is no specified 'sort' in the params, then trigger the redirect variable
     elsif(session.has_key?(:sort) )
       params[:sort] = session[:sort]
       @redirect = 1
     end
 
+    # if there are specified ratings to filter by in params, then store that in the session and perform the filter
     if(params[:ratings] != nil)
       session[:ratings] = params[:ratings]
       @checked_ratings = params[:ratings].keys
       @movies = Movie.find(:all, :order => params[:sort]).select { |m| @checked_ratings.include?(m.rating) }
+    # but if there are no specified 'ratings' in the params, then trigger the redirect variable
     elsif(session.has_key?(:ratings) )
       params[:ratings] = session[:ratings]
       @redirect =1
     end
     
+    # if a redirect has been triggered, then send the index back to movies_path with the restored params
     if(@redirect ==1)
       flash.keep
       redirect_to movies_path(:sort=>params[:sort], :ratings =>params[:ratings] )
